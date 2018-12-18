8e788540dc63dcb6a0a0e55c5a0395c0f622c3ec
esc_jmelo <esc_jmelo@192-168-56-1.privateip>
2018-12-18 14:40:04 -0200

    Criação do gateway

---
+++ src/main/java/br/com/uoldiveo/portal/gateway/uoldiveoportalgateway/UoldiveoPortalGatewayApplication.java
@@ -0,0 +1,14 @@
+package br.com.uoldiveo.portal.gateway.uoldiveoportalgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UoldiveoPortalGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UoldiveoPortalGatewayApplication.class, args);
+	}
+
+}
+
