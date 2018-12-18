712c5976ae6325ca8a312132a0bd6a93e6afbf14
esc_jmelo <esc_jmelo@192-168-56-1.privateip>
2018-12-18 14:51:14 -0200

    Request no microservice de login

--- src/main/java/br/com/uoldiveo/portal/gateway/uoldiveoportalgateway/UoldiveoPortalGatewayApplication.java
+++ src/main/java/br/com/uoldiveo/portal/gateway/uoldiveoportalgateway/UoldiveoPortalGatewayApplication.java
@@ -2,13 +2,13 @@ package br.com.uoldiveo.portal.gateway.uoldiveoportalgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class UoldiveoPortalGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UoldiveoPortalGatewayApplication.class, args);
 	}
-
 }
-
